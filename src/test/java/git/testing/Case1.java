3b9fa1a8ae7a26506d2751631b9db93dc8ecef31
SSR <sheelamsatishreddy@gmail.com>
2024-11-20 11:46:38 +0530

    this is my first commit

---
+++ src/test/java/git/testing/Case1.java
@@ -0,0 +1,16 @@
+package git.testing;
+
+import org.testng.annotations.Test;
+
+public class Case1 {
+
+	@Test
+	public void case1test1() {
+		System.out.println("Case1 test1");
+	}
+	public void case1test2() {
+		System.out.println("Case1 test2");
+	}
+	
+	
+}
